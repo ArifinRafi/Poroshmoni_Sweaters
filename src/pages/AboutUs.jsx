5c51a23a849bb223107a285018a75206f416799d
ArifinRafi <ahmedrafi364@gmail.com>
2024-02-03 17:39:11 +0600

    commit

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => {
                         <h1 className='text-3xl mt-24 text-blue-800 font-semibold'>About Us</h1>
                         <h1 className='text-xl mb-12 text-gray-700 font-semibold'>We Are Providing Service Since 2003 </h1>
                          <br />
-                        <div className='grid  lg:grid-cols-2 justify-items-center'>
+                        <div className='grid gap-8 lg:grid-cols-2 justify-items-center'>
                         <div className='bg-blue-900 flex items-center justify-center lg:m-0 m-4  lg:w-[200px] shadow-2xl w-[20rem] h-[250px] text-white'>
                         <div className='mx-4'> <h1 className='text-2xl flex justify-center pt-4 font-semibold'>20+ </h1 >  <span>Years of Experiences</span> <br /><div className='flex justify-center items-center'>
                         <GrAchievement className='lg:w-[80px] w-[80px] h-[50px] lg:mt-2 mt-16  ' /></div></div>
@@ -24,16 +24,16 @@ const AboutUs = () => {
                        <ul className='flex justify-center lg:w-[190px]'>
                         <IoMdCheckmarkCircleOutline className='text-blue-900 w-[30px] h-[20px]' /> <div>100% export oriente</div>
                         </ul>
-                        <ul className='flex justify-center lg:w-[210px]'>
+                        <ul className='flex justify-center lg:ml-0 ml-4 lg:w-[210px]'>
                         <IoMdCheckmarkCircleOutline className='text-blue-900 w-[30px] h-[20px]' /> <div>23+ years of experince</div>
                         </ul>
                         <ul className='flex justify-center lg:w-[192px]'>
                         <IoMdCheckmarkCircleOutline className='text-blue-900  w-[30px] h-[20px]' /> <div>Modern machineries</div>
                         </ul>
-                        <ul className='flex justify-center lg:w-[215px]'>
+                        <ul className='flex justify-center lg:ml-0 ml-5 lg:w-[215px]'>
                         <IoMdCheckmarkCircleOutline className='text-blue-900  w-[30px] h-[20px]' /> <div>Quality Control System</div>
                         </ul>
-                        <ul className='flex justify-center lg:w-[230px]'>
+                        <ul className='flex justify-center lg:ml-0 ml-8 lg:w-[230px]'>
                         <IoMdCheckmarkCircleOutline className='text-blue-900  w-[30px] h-[20px]' /> <div>Highly Professional Team</div>
                         </ul>
                        </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,16 +8,16 @@ const Services = () => {
         return (
                 <div className='grid grid-cols-1 lg:grid-cols-3 gap-6
                  max-w-screen-2xl mx-auto '>
-                    <div className=' hover:bg-blue-900 shadow-2xl border-blue-900 text-blue-900 hover:text-white  lg:mx-12 w-full flex flex-col  items-center rounded-3xl h-[300px]'><FaBookOpen className='mt-4 w-[100px] h-[50px]' /><h1 className='font-semibold  text-2xl'>Our Story</h1><br />
+                    <div className=' hover:bg-blue-900 shadow-2xl border-blue-900 text-blue-900 hover:text-white   w-full flex flex-col  items-center rounded-3xl h-[18rem] w-[20rem]'><FaBookOpen className='mt-4 w-[100px] h-[50px]' /><h1 className='font-semibold  text-2xl'>Our Story</h1><br />
                     
                     <h3 className='px-4'>We the Proshmoni Sweater Ltd., like to introduce ourself as a sweater manufacturer factory in Dhaka, Bangladesh. We ensure you the quality and on time Delivery.</h3>
                     </div>
 
-                    <div className=' hover:bg-blue-900  shadow-2xl border-blue-900 text-blue-900 hover:text-white  lg:mx-12 w-full flex flex-col  items-center rounded-3xl h-[300px]'><MdProductionQuantityLimits className='mt-4 w-[100px] h-[50px]' /><h1 className='font-semibold  text-2xl'>Product Items</h1><br />
+                    <div className=' hover:bg-blue-900  shadow-2xl border-blue-900 text-blue-900 hover:text-white  w-full flex flex-col  items-center rounded-3xl h-[18rem] w-[20rem]'><MdProductionQuantityLimits className='mt-4 w-[100px] h-[50px]' /><h1 className='font-semibold  text-2xl'>Product Items</h1><br />
                     <h3 className='px-4'>We make all kinds of Sweaters maintaining the international Standard</h3>
                     </div>  
 
-                    <div className=' hover:bg-blue-900 shadow-2xl  text-blue-900 hover:text-white lg:mx-12 w-full flex flex-col  items-center rounded-3xl h-[300px]'><MdOutlineReduceCapacity className='mt-4 w-[100px] h-[50px]' /><h1 className='font-semibold text-2xl'>Production Capacity</h1><br />
+                    <div className=' hover:bg-blue-900 shadow-2xl  text-blue-900 hover:text-white w-full flex flex-col  items-center rounded-3xl h-[18rem] w-[20rem]'><MdOutlineReduceCapacity className='mt-4 w-[100px] h-[50px]' /><h1 className='font-semibold text-2xl'>Production Capacity</h1><br />
                     <h3 className='px-4'>Currently our production capacity is 7500 DZ per month. Also, we have 45000 sft. floor space with emergency exit stairs. We have 1000 male and female workers and 35 office staffs in our team </h3>
                     </div>
 

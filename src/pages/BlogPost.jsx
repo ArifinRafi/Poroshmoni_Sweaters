adc28f51e85fd393c64a41a1213e3ce8e01e4d1d
ArifinRafi <ahmedrafi364@gmail.com>
2024-02-21 17:30:34 +0600

    blogPost

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import blogs from '../JsonData/BlogData.json';
+import women from '../assets/images/women-garments.jpg'
+import jaq from '../assets/images/jaq.jpg'
+import industry from '../assets/images/sweter_industry.jpg'
 
 const BlogPost = () => {
-        const {index} = useParams();
+    const { index } = useParams();
 
-            const blog = blogs[index];
-        return (
-                <div className='flex max-w-screen-2xl mx-auto flex-col justify-center items-center'>
-                <img src={blog.img} alt="Shoes" />
-                 <h2 className='text-bold text-4xl'>{blog.title}</h2>  
-                 <p className='text-sm text-gray-800'>{blog.content}</p> 
+    const blogImages = [
+        { img: women, title: 'Impact of Women', content: 'Content for Impact of Women blog post...' },
+        { img: jaq, title: 'The rise of Jaqcuard machines', content: 'Content for The rise of Jaqcuard machines blog post...' },
+        { img: industry , title: 'Impact of Women', content: 'Content for Impact of Women blog post...' },
+    ];
 
-                  <div className='grid lg:grid-cols-3 max-w-screen-2xl mx-auto justify-items-center gap-8 mt-12'>
-                        {blogs.map((blog, index)=> (
-                                <div key={index} className="card w-96 bg-base-100 shadow-xl">
-                                <figure><img src={blog.img} alt="Shoes" /></figure>
-                                <div className="card-body">
-                                <h2 className="card-title">{blog.title}</h2>
-                                <p>{blog.description}</p>
-                                
-                        
-                         </div>
-                         </div> 
+    const blogImage = blogImages[index];
+    const blog = blogs[index];
 
-                        ))}
-                        
-                 </div>    
-                </div>
-        );
+    return (
+        <div className='max-w-screen-2xl mx-auto flex flex-col justify-center items-center'>
+            <img className='w-1/2' src={blogImage.img} alt="" />
+            <h2 className='text-3xl'>{blog.title}</h2>
+            <p>{blog.content}</p>
+            {/* You can add the image here if needed */}
+            {/* <img src={blog.img} alt={blog.title} /> */}
+        </div>
+    );
 };
 
 export default BlogPost;

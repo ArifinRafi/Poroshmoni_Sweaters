751e3c6d9a03bb61d2754c952bdffa8554cbf494
ArifinRafi <ahmedrafi364@gmail.com>
2024-02-27 10:21:48 +0600

    about us margin

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -16,7 +16,7 @@ const AboutUs = () => {
                         <h1 className='text-xl mb-12 text-gray-700 font-semibold'>We Are Providing Service Since 2003 </h1>
                          <br />
                         <div className='grid gap-8 lg:grid-cols-2 justify-items-center'>
-                        <div className='bg-blue-900 flex items-center justify-center lg:m-0 m-4  lg:w-[200px] shadow-2xl w-[20rem] h-[250px] text-white'>
+                        <div className='bg-blue-900 flex items-center justify-center lg:m-0  lg:w-[200px] shadow-2xl w-[20rem] h-[250px] text-white'>
                         <div className='mx-4'> <h1 className='text-2xl flex justify-center pt-4 font-semibold'>20+ </h1 >  <span>Years of Experiences</span> <br /><div className='flex justify-center items-center'>
                         <GrAchievement className='lg:w-[80px] w-[80px] h-[50px] lg:mt-2 mt-16  ' /></div></div>
                        
